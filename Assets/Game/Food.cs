acaca463f92c3ba6f64785ab4d51a6ea4ceb56cd
jez <jeremy+github@jeremystevens.net>
2019-12-08 16:04:28 +0000

    implemented initial steps for placeable food

---
+++ Assets/Game/Food.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Food : MonoBehaviour {
+
+    public Transform bouncingObject;
+    private Vector3 initialObjPos;
+    private float timer;
+
+    void Start() {
+        initialObjPos = bouncingObject.position;
+        timer = 0;
+    }
+
+    void Update() {
+        timer += Time.deltaTime;
+        timer = timer % (2 * Mathf.PI);
+        bouncingObject.transform.position = new Vector3(initialObjPos.x, initialObjPos.y + Mathf.Sin(timer), initialObjPos.z);
+    }
+}
---
+++ Assets/Game/InteractionController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class InteractionController : MonoBehaviour {
+    public LayerMask inputLayerMask;
+    public GameObject placedObject;
+
+    void Update() {
+        if (Input.GetButtonDown("Fire1")) {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity, inputLayerMask)) {
+                var position = hit.point;
+                Instantiate(placedObject, position, Quaternion.identity);
+            }
+        }
+    }
+}
