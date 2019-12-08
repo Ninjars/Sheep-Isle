53d3e3c49f4357e51641fdb8ec272a2d10c82352
jez <jeremy+github@jeremystevens.net>
2019-12-08 23:51:26 +0000

    adjusted food bounce

--- Assets/Game/Food.cs
+++ Assets/Game/Food.cs
@@ -1,10 +1,10 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Food : MonoBehaviour {
 
     public Transform bouncingObject;
+    public float bounceHeight = 0.5f;
+
     private Vector3 initialObjPos;
     private float timer;
 
@@ -16,6 +16,6 @@ public class Food : MonoBehaviour {
     void Update() {
         timer += Time.deltaTime;
         timer = timer % (2 * Mathf.PI);
-        bouncingObject.transform.position = new Vector3(initialObjPos.x, initialObjPos.y + Mathf.Sin(timer), initialObjPos.z);
+        bouncingObject.transform.position = new Vector3(initialObjPos.x, initialObjPos.y + bounceHeight * Mathf.Sin(timer), initialObjPos.z);
     }
 }
