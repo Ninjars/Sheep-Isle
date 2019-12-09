6eb8f3b018e954eb3906eea212da2c7e7487d0e7
jez <jeremy+github@jeremystevens.net>
2019-12-09 00:34:07 +0000

    added breathing effect to the sheep

--- Assets/Game/Agents/WanderingAgent.cs
+++ Assets/Game/Agents/WanderingAgent.cs
@@ -12,19 +12,24 @@ namespace Game {
         public float wanderDistance = 8;
 		public float minActionDelaySeconds = 3;
 		public float maxActionDelaySeconds = 10;
-		public bool debugDraw = false;
+        public float breathCycleTime = 2;
+        public Transform body;
+        public float breathHeight = 0.05f;
+        private Vector3 initialObjPos;
+        private float breathTimer = -1;
+        private float breathCycleCurrent;
 
 		void Awake() {
 			base.init();
+            initialObjPos = body.localPosition;
 		}
 
-		// Use this for initialization
 		void Start () {
 			moveToRandomPoint();
 		}
 		
-		// Update is called once per frame
 		void Update () {
+            updateBreathing();
 			currentDelayTime += Time.deltaTime;
 			if (currentDelayTime > nextActionDelay) {
                 moveToRandomPoint();
@@ -32,7 +37,18 @@ namespace Game {
 			}
 		}
 
-		private void moveToRandomPoint() {
+        private void updateBreathing() {
+            breathTimer -= Time.deltaTime;
+            if (breathTimer < 0) {
+                breathCycleCurrent = breathCycleTime * UnityEngine.Random.Range(0.8f, 1.2f);
+                breathTimer = breathCycleCurrent;
+            }
+            var fraction = 1 - breathTimer / breathCycleCurrent;
+            var value = fraction * (2 * Mathf.PI);
+            body.localPosition = new Vector3(initialObjPos.x, initialObjPos.y + breathHeight * Mathf.Sin(value), initialObjPos.z);
+        }
+
+        private void moveToRandomPoint() {
             Vector3 targetPosition = Game.Utils.RandomNavSphere(transform.position, wanderDistance, -1);
 			nextActionDelay = (float)UnityEngine.Random.Range(minActionDelaySeconds, maxActionDelaySeconds);
 			MoveToLocation(targetPosition);
